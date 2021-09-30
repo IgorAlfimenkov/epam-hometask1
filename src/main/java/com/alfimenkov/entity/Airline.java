4c5b10a4ec34bf79d65abc4704a34a12327cf3e4
IgorAlfimenkov <igoralfimenkov@gmail.com>
2021-09-30 17:59:30 +0300

    define main entities of application

--- src/main/java/com/alfimenkov/entity/Airline.java
+++ src/main/java/com/alfimenkov/entity/Airline.java
@@ -1,4 +1,31 @@
 package com.alfimenkov.entity;
 
+import java.util.ArrayList;
+
 public class Airline {
+
+    private ArrayList <Airplane> airplanes;
+
+    public Airline(ArrayList<Airplane> airplanes) {
+        this.airplanes = airplanes;
+    }
+
+    public Airline() {
+
+    }
+
+    public ArrayList<Airplane> getAirplanes() {
+        return airplanes;
+    }
+
+    public void setAirplanes(ArrayList<Airplane> airplanes) {
+        this.airplanes = airplanes;
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "airplanes=" + airplanes +
+                '}';
+    }
 }
